7f018716dab0831a5503fd2c7850d6303f764181
Andrew Yasso <andrew.yasso@gmail.com>
2018-05-24 12:04:35 -0600

    page creation

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :body, :author)
+    end
+
+end
